470a8146f3e67721c9952f975d89b9d723d9d3ad
Carlos-Larin <nekokar701@gmail.com>
2024-09-04 21:28:45 -0600

    nuevo

---
+++ main.py
@@ -0,0 +1,6 @@
+# import modulo
+#from modulo import funcion
+
+import matematicas
+import matematicas.aritmetica
+import matematicas.aritmetica import sumar
---
+++ matematica/aritmetica.py
@@ -0,0 +1,16 @@
+def sumar(n1,n2):
+    n1+n2
+
+def restar(n1,n2):
+    n1-n2
+
+def multiplicar(n1,n2):
+    n1*n2
+
+def dividir(n1,n2):
+    n1/n2
+
+#rando y rando por rango
+from random import random, randrange
+
+print(random())
